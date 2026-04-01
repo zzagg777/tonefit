88f1a222e4e803197fd5529301a7fbefa28c05d6
min <zzagg777@gmail.com>
2026-04-01 14:10:27 +0900

    feat: SCRUM-8 최초테스트

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+// SCRUM-8 로그인 페이지 작업 시작
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
